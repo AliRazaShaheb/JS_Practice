c1c9197b9e0055abc71ef4dc47310ace23276b88
ALI RAZA SHAHEB <89347148+AliRazaShaheb@users.noreply.github.com>
2022-07-25 17:31:59 +0530

    added general questions

---
+++ src/JSque/generalQuestions.js
@@ -0,0 +1,62 @@
+/* 
+what you have learned today/yesterday?
+ans:
+programing related and none programming
+
+if !programming
+
+  how to courier technical things
+  I courier a laptop from my prev. company.
+  - you have to pack things up
+  - they have safety kit - you have additon rs. 300/-
+  - you can insurance also 
+  - if anything happen in your product
+
+  I learned also about How to file a case in SDM office.
+    - its really horrible
+
+learning about DSA
+algorighm, time complexity
+data structure - Stack & Que 
+
+answser - I always learn something new. use
+
+
+*/
+
+/*
+Q. What excites or interests you about coding?
+human tend to productive, creative, problem solver
+in day to day life we always face problems.
+lets assume these are task.
+task is what you want to build someting or want to solve
+for completion of certern task you need follow certain steps
+
+In Computer Program 
+your can solve task in meaningful logical steps,
+
+I like solving problems
+these are few things that excite me about programming
+thourgh programming 
+we can solve problems  
+interactively, 
+logical steps,
+we can automate our task 
+we can call these step for later use
+its a faster way to solve problem
+
+
+
+A. You can do your task in logical steps, automate your task
+*/
+
+/* Q. What is a recent technical challenge you experienced 
+and how did you solve it?
+
+pwa install promp to be removed. it was a challenge for me.
+removal of install promp
+I read lots of articals. most of it was out dated.
+I finally managed to do. it was lots of hit and trial.
+there is an event call 'beforeinstalledpromp' on this event just prevent the default that it.
+
+*/
--- src/JSque/practicalQue.js
+++ src/JSque/practicalQue.js
@@ -124,3 +124,140 @@ now here foo is pointing box2
 now we just assign key 'x' to box2 through foo without any value
 so it assign default javascript value 'undefined'
 */
+
+import AllPromises from "./JSque/JSPromises";
+
+console.clear();
+/* Q6.  What is the value of foo.length?
+console.log('one');
+setTimeout(function() {
+  console.log('two');
+}, 0);
+Promise.resolve().then(function() {
+  console.log('three');
+})
+console.log('four');
+
+== my Answer== (correct)
+
+one
+four
+three
+two
+
+
+== How ==
+first run synchrounous code
+then javascript promise
+then browser API
+
+lets analyse the above code
+1. console.log('one'); // synchronous code
+2. console.log('four'); // synchronous code
+3. here already resolved then .then(calling resolved value)
+4. browser API
+*/
+
+/*07.  What is the difference between these four promises?
+
+doSomething().then(function () {
+  return doSomethingElse();
+});
+
+doSomething().then(function () {
+  doSomethingElse();
+});
+
+doSomething().then(doSomethingElse());
+
+doSomething().then(doSomethingElse);
+
+== my Answer== 
+lets assume do something is resolved
+lets doSomethingElse is returning something
+
+case 1
+function didn't have parameter this means
+resolved value is not get used. 
+only doSomething value if return;
+will run after resolved
+
+case 2
+same as case 1
+
+case 3
+if not callback in then 
+function in then will execute immediately and will show
+no matter it resolved or not.
+
+
+case 4
+same as case 1 and 2
+without callback and parameter... resolved value didn't get used
+it will show the resolved value
+witout calling the function. it will show the resolved value
+
+
+== How ==
+*/
+
+const [promise1] = AllPromises;
+// promise1(6)
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const benamfunc = (val) => {
+//   return console.log(val)
+// };
+
+// promise1(6)
+//   .then(benamfunc)
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+/* Q.7 What will the code below output to the console and why?
+(function(){
+  var a = b = 3;
+})();
+
+console.log("a defined? " + (typeof a !== 'undefined'));
+console.log("b defined? " + (typeof b !== 'undefined'));
+== my Answer== 
+a defined ? true (wrong)
+b defined ? true (wrong)
+
+my calculation are correct
+since the abvoe question confuse me so, got wrong
+
+a defined ? true (correct)
+b defined ? true (correct)
+reason ===
+please see :- self invoked function
+var a ; var b
+has not access outside of a function
+here...
+typeof a is undefined
+typeof b is undefined
+
+so, it will return false
+
+== How == 
+here value get passed from right to left
+b is not defined with varible prefixes, in strict mode it will give error.
+without strict mode, interpreter will give default
+var value to it b=3; so here 
+var b = 3
+now 
+var a == b
+so type of a & b are number
+
+a defined ? true
+b defined ? true
+
+
+*/
