43d80b39530a26e0a23b0b1c103aaf20932131ae
ALI RAZA SHAHEB <89347148+AliRazaShaheb@users.noreply.github.com>
2022-10-27 11:12:22 +0530

    promise correction

--- src/general/index.js
+++ src/general/index.js
@@ -1,73 +1,14 @@
-const myPromise1 = new Promise((resolve, reject)=>{
-  resolve("my promise 1 resolved")
-})
+const name = "ali";
 
-// working ok
-/*
-myPromise1
-.then(resolveData=>console.log(resolveData))
-.catch(rejectData => console.log(rejectData))
-*/
-
-const myPromise2 = new Promise((resolve, reject)=>{
-  reject("my promise 2 rejected")
-})
-
-// working ok
-/*
-myPromise2
-.then(resolveData=>console.log(resolveData))
-.catch(rejectData => console.log(rejectData))
-*/
-
-
-const myPromise3 = async()=>{
-  return 'my promise 3 resolve'
+function displayName() {
+  console.log(this.name);
 }
 
-// working ok
-// myPromise3().then(resolveData => console.log(resolveData));
-
-
-const myPromise4 = async()=>{
-  return 'my promise 4 resolve'
-}
-
-const getData = async()=>{
-  const data = await myPromise4()
-  console.log(data)
+const person = {
+  name: "arz",
+  method: displayName.bind(this)
 };
 
-// getData();
-// console.log(myPromise4())
+person.method(); // TypeError
 
-
-const myPromise5 = ()=>{
-  return new Promise((resolve, reject)=>{
-    setTimeout(()=>{resolve("promise 5 resolved")}, 3000)
-  })
-}
-
-const myPromise6 = ()=>{
-  return new Promise((resolve, reject)=>{
-    setTimeout(()=>{resolve("promise 6 resolved")}, 5000)
-  })
-}
-
-
-const getData1 = async()=>{
-  
-  console.log('waiting promise 5 ...')
-  const data = await myPromise5();
-  console.log(data);
-  
-  
-  
-  console.log('waiting promise 6 ...')
-  const data2 = await myPromise6();
-  console.log(data2)
-  
-  
-};
-
-getData1()
+//if strict mode its undefined
